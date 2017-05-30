7819df5e184f8706510b5687715d284ab8c0b289
Yuriy Flyud <yuflyud@gmail.com>
2017-05-30 23:18:23 +0300

    Added logging for listener

--- activemq-webapp/src/main/java/com/hevelian/activemq/servlet/ServletContextHolderInitializer.java
+++ activemq-webapp/src/main/java/com/hevelian/activemq/servlet/ServletContextHolderInitializer.java
@@ -3,6 +3,9 @@ package com.hevelian.activemq.servlet;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Servlet Context listener that stores current Servlet Context instance to a
  * {@link ServletContextHolder}.
@@ -11,14 +14,19 @@ import javax.servlet.ServletContextListener;
  *
  */
 public class ServletContextHolderInitializer implements ServletContextListener {
+	private static final Logger LOG = LoggerFactory.getLogger(ServletContextHolderInitializer.class);
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
 		ServletContextHolder.setServletContext(sce.getServletContext());
+		LOG.debug(String.format("Servlet context with name '%s' is set as current",
+				sce.getServletContext().getServletContextName()));
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		ServletContextHolder.removeServletContext();
+		LOG.debug("Current Servlet Context is set to null");
+
 	}
 }
